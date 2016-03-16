fa84844556d135bac41eeded11b31639e972f450
cinkowski <cinq2ez@gmail.com>
2016-03-16 20:52:47 +0100

    Modify app.py to serve index.html with compiled ember_app

--- server/app.py
+++ server/app.py
@@ -2,9 +2,9 @@ from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
-# @app.route("/")
-# def index():
-#     return render_template('index.html')
+@app.route("/")
+def index():
+    return send_from_directory('static', 'index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
