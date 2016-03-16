1194937093da94c9b757bee2ac26e2825e624447
cinkowski <cinq2ez@gmail.com>
2016-03-16 21:15:53 +0100

    Add test endpoint for future use

--- server/app.py
+++ server/app.py
@@ -1,10 +1,18 @@
 from flask import Flask, send_from_directory
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
 @app.route("/")
 def index():
     return send_from_directory('static', 'index.html')
+    
+class User(Resource):
+    def get(self):
+        return {'hello': 'world'}
+        
+api.add_resource(User, '/user')
 
 if __name__ == "__main__":
     app.run(debug=True)
