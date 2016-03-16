60c88dcf4889a81f039d9a45b73cdf2b3a0c3b16
cinkowski <cinq2ez@gmail.com>
2016-03-16 21:46:13 +0100

    Add restadapter, Prepare mock data for /users

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, jsonify
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -8,11 +8,25 @@ api = Api(app)
 def index():
     return send_from_directory('static', 'index.html')
     
-class User(Resource):
-    def get(self):
-        return {'hello': 'world'}
+class User():
+    def __init__(self, name):
+        self.name = name
         
-api.add_resource(User, '/user')
+    @property
+    def serialize(self):
+        return {
+            'name': self.name
+        }
+
+user1 = User('andrzej')
+user2 = User('adam')
+user3 = User('janusz')
+userArr = [user1, user2, user3]
+class Users(Resource):
+    def get(self):
+        return jsonify({'user': [i.serialize for i in userArr]})
+        
+api.add_resource(Users, '/users')
 
 if __name__ == "__main__":
     app.run(debug=True)
