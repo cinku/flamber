c5a0d9c5b589d9f8669f458f45c8d216c1afa077
cinkowski <cinq2ez@gmail.com>
2016-03-17 21:39:30 +0100

    Modify app.py to load config

--- server/app.py
+++ server/app.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.from_object('config')
 api = Api(app)
 db = SQLAlchemy(app)
 
--- server/config.py
+++ server/config.py
@@ -1,6 +1,4 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-print(basedir)
-
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'flamber.db')
