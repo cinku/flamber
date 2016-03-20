61c380c214bb022fc03af123fa80f1e03f07fdfa
cinkowski <cinq2ez@gmail.com>
2016-03-20 14:54:45 +0100

    Add simple form for entering new flames

--- client/app/components/flame-form.js
+++ client/app/components/flame-form.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    actions: {
+        postFlame() {
+            let text = this.get('flame');
+            console.log(text);
+        }
+    }
 });
