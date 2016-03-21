0da451b7223378347293446b5949641da931e882
cinkowski <cinq2ez@gmail.com>
2016-03-21 20:35:21 +0100

    Modify user by adding password, Add migration corresponding with password change

--- server/app.py
+++ server/app.py
@@ -23,6 +23,7 @@ class User(db.Model):
     name = db.Column(db.String(64), unique=True)
     username = db.Column(db.String(64), unique=True)
     email = db.Column(db.String(64), unique=True)
+    password_hash = db.Column(db.String(128))
     flames = db.relationship('Flame', backref='user', lazy='dynamic')
         
     @property
