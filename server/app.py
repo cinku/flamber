c1e1da94abd62d7d53098b0a88befdb3bf6cf754
cinkowski <cinq2ez@gmail.com>
2016-03-21 22:04:50 +0100

    Add marshmallow schemas for beautful api

--- server/app.py
+++ server/app.py
@@ -3,7 +3,9 @@ from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from marshmallow import Schema, fields
 from datetime import datetime
+
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -26,16 +28,12 @@ class User(db.Model):
     password_hash = db.Column(db.String(128))
     flames = db.relationship('Flame', backref='user', lazy='dynamic')
         
-    @property
-    def serialize(self):
-        return {
-            'id': self.id,
-            'name': self.name,
-            'username': self.username,
-            'email': self.email,
-            # TODO start using marshmallow bby gurl
-            # 'flames': [i.serialize for i in Flame.query.filter_by(user_id=self.id)]
-        }
+class UserSchema(Schema):
+    id = fields.Integer()
+    name = fields.String()
+    username = fields.String()
+    email = fields.Email()
+    flames = fields.Nested('FlameSchema', exclude=['user'], many=True)
         
 class Flame(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -48,23 +46,20 @@ class Flame(db.Model):
         if pub_date is None:
             self.pub_date = datetime.utcnow()
         self.user_id = user_id
-    
-    @property
-    def serialize(self):
-        return {
-            'id': self.id,
-            'text': self.text,
-            'pub_date': self.pub_date,
-            'user': self.user_id
-        }
+
+class FlameSchema(Schema):
+    id = fields.Integer()
+    text = fields.String()
+    pub_date = fields.DateTime()
+    user = fields.Nested('UserSchema', only=['id'])
 
 class Users(Resource):
     def get(self):
-        return jsonify({'user': [i.serialize for i in User.query.all()]})
+        return jsonify({'user': [UserSchema().dump(i).data for i in User.query.all()]})
         
 class UsersUpdate(Resource):
     def get(self, user_id):
-        return jsonify({'user': User.query.get(user_id).serialize})
+        return jsonify({'user': UserSchema().dump(User.query.get(user_id)).data})
         
 class Flames(Resource):
     def post(self):
@@ -74,11 +69,11 @@ class Flames(Resource):
         db.session.commit()
         return 200
     def get(self):
-        return jsonify({'flame': [i.serialize for i in Flame.query.all()]})
+        return jsonify({'flame': [FlameSchema().dump(i).data for i in Flame.query.all()]})
         
 class FlamesUpdate(Resource):
     def get(self, flame_id):
-        return jsonify({'flame': Flame.query.get(flame_id).serialize})
+        return jsonify({'flame': FlameSchema().dump(Flame.query.get(flame_id)).data})
         
 api.add_resource(Users, '/users')
 api.add_resource(Flames, '/flames')
