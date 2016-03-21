3b0a5136fd2c63568511bf714d5520a70b52aa6d
cinkowski <cinq2ez@gmail.com>
2016-03-21 20:32:27 +0100

    Add migration support and initial migrations

--- server/app.py
+++ server/app.py
@@ -1,12 +1,17 @@
 from flask import Flask, send_from_directory, jsonify, request
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 from datetime import datetime
 
 app = Flask(__name__)
 app.config.from_object('config')
 api = Api(app)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
---
+++ server/manage.py
@@ -0,0 +1,4 @@
+from app import app, db, manager, migrate
+
+if __name__ == "__main__":
+    manager.run()
