0492d3002d8d04cdfef8f17f3001b69ae481fdde
cinkowski <cinq2ez@gmail.com>
2016-03-22 20:10:18 +0100

    Add custom authenticator

--- client/app/components/login-form.js
+++ client/app/components/login-form.js
@@ -1,10 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    session: Ember.inject.service('session'),
     actions: {
         authenticate() {
             let { identification, password } = this.getProperties('identification', 'password');
-            this.get('session').authenticate('authenticator:oauth2', identification, password).catch((reason) => {
+            this.get('session').authenticate('authenticator:custom', identification, password).catch((reason) => {
                 this.set('errorMessage', reason.error || reason);
             });
         }        
