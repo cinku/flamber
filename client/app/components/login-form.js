0a2930ddcf7cc5d7122407e9bb2498d23f765ee7
cinkowski <cinq2ez@gmail.com>
2016-03-22 20:00:51 +0100

    Add basic login form and authentication action

--- client/app/components/login-form.js
+++ client/app/components/login-form.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    actions: {
+        authenticate() {
+            let { identification, password } = this.getProperties('identification', 'password');
+            this.get('session').authenticate('authenticator:oauth2', identification, password).catch((reason) => {
+                this.set('errorMessage', reason.error || reason);
+            });
+        }        
+    }
 });
