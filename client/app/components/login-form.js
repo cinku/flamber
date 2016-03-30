16102450c2867eebf2cbee180528c2deec70d734
cinkowski <cinq2ez@gmail.com>
2016-03-30 21:17:51 +0200

    Add login-form to index

--- client/app/components/login-form.js
+++ client/app/components/login-form.js
@@ -4,10 +4,9 @@ export default Ember.Component.extend({
     session: Ember.inject.service('session'),
     actions: {
         authenticate() {
+            console.log('asd');
             let { identification, password } = this.getProperties('identification', 'password');
-            this.get('session').authenticate('authenticator:custom', identification, password).catch((reason) => {
-                this.set('errorMessage', reason.error || reason);
-            });
+            this.get('session').authenticate('authenticator:custom', identification, password);
         }        
     }
 });
