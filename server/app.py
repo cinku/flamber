938901ff9c2d72a3abdd253c680c9ca429679602
Marcin Marczak <marcin.marczak@capgemini.com>
2016-03-31 11:31:52 +0200

    Add generate token method and Naive verify_password method

--- server/app.py
+++ server/app.py
@@ -38,6 +38,16 @@ class User(db.Model):
     def verify_password(self, password):
         return pwd_context.verify(password, self.password_hash)
         
+    def generate_auth_token(self, expiration=600):
+        s = Serializer(app.config['SECRET_KEY'], expires_in=expiration)
+        return s.dumps({'id': self.id})
+        
+@auth.verify_password
+def verify_password(username_or_token, password):
+    user = User.query.filter_by(username = "asd").first()
+    g.user = user
+    return True
+        
 class UserSchema(Schema):
     id = fields.Integer()
     name = fields.String()
