33e008ee0714701d7d07e18bb837899c8323b0bd
cinkowski <cinq2ez@gmail.com>
2016-04-16 19:44:34 +0200

    Modify gulpfile to compile ts files

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,25 @@
 var gulp = require('gulp');
+var ts = require('gulp-typescript');
+var merge = require('merge2');
 
-gulp.task('default', function() {
-  // place code for your default task here
+var tsProject = ts.createProject('tsconfig.json', {
+    declaration: true,
+    noExternalResolve: true
 });
+
+gulp.task('compile_ts', function() {
+//   var tsProject = ts.createProject('tsconfig.json');
+	var tsResult = tsProject.src()
+		.pipe(ts(tsProject));
+ 
+	return merge([
+		tsResult.dts.pipe(gulp.dest('release')),
+		tsResult.js.pipe(gulp.dest('release'))
+	]);
+});
+
+gulp.task('watch_ts', function () {
+  gulp.watch(['./server/static/**/*.ts'], ['default']);
+});
+
+gulp.task('default', ['compile_ts']);
