6485806c2b9ba37482177fbb893db9fb01af23a3
cinkowski <cinq2ez@gmail.com>
2016-04-17 20:57:27 +0200

    Add naive implementation of angular app in ts

--- server/static/client/app.ts
+++ server/static/client/app.ts
@@ -1,3 +1,14 @@
-'use strict';
-
-angular.module('flamberApp', []);
+$((): void =>  {
+    let flamberApp: angular.IModule = angular.module('flamberApp', ['ui.router', 'ngMaterial']);
+    
+    flamberApp.config(['$urlRouterProvider', '$stateProvider', ($urlRouterProvider: ng.ui.IUrlRouterProvider,
+                                                                $stateProvider: ng.ui.IStateProvider) => {
+        // router
+    }]);
+    
+    flamberApp.run(() => {
+       // run 
+    });
+    
+    angular.bootstrap(document, ['flamberApp']);
+});
