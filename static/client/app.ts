058778da1201844203a63cddecaff1ec2b1d08a3
cinkowski <cinq2ez@gmail.com>
2016-04-20 20:39:43 +0200

    Add routes for sample views, Modify tsconfig.json after reorganization of project

--- static/client/app.ts
+++ static/client/app.ts
@@ -3,7 +3,14 @@ $((): void =>  {
     
     flamberApp.config(['$urlRouterProvider', '$stateProvider', ($urlRouterProvider: ng.ui.IUrlRouterProvider,
                                                                 $stateProvider: ng.ui.IStateProvider) => {
-        // router
+        $stateProvider.state('home', {
+            url: '/home',
+            template: './partials/home.html'
+        })
+        .state('about', {
+            url: '/about',
+            template: './partials/about.html'
+        });
     }]);
     
     flamberApp.run(() => {
