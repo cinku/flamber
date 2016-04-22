b8e0d0919ece4794e90979a0363c4aca74e00c41
cinkowski <cinq2ez@gmail.com>
2016-04-22 20:57:00 +0200

    Modify homeController

--- static/client/controllers/homeController.ts
+++ static/client/controllers/homeController.ts
@@ -1,11 +1,12 @@
 namespace main.controllers { 
-        interface Flames {
+        export interface Flames {
             id: number;
             name: string;
             pub_date: string;
-        }   
+        }
+           
     export class HomeController {
-        private flames: Flames[];
+        flames: Flames[];
         
         constructor(private $scope: ng.IScope, private $http: ng.IHttpService){
             $http.get('/flames').success((response: Flames[]) => {
