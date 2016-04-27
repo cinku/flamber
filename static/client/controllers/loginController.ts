7ceb8a99443c4853118db6f97e528db22347a44b
cinkowski <cinq2ez@gmail.com>
2016-04-27 20:20:42 +0200

    Add naive implementation of LoginController

--- static/client/controllers/loginController.ts
+++ static/client/controllers/loginController.ts
@@ -1,3 +1,10 @@
 namespace main.controllers {
-    
+    export class LoginController {
+        username: string;
+        password: string;
+        
+        constructor(private $scope: ng.IScope, private $http: ng.IHttpService){
+
+        }
+    }
 }
