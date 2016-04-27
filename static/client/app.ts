014c97efe0d2022526a70caf0b95022ec1c0436e
cinkowski <cinq2ez@gmail.com>
2016-04-27 20:11:38 +0200

    Remove HomeController for the time being

--- static/client/app.ts
+++ static/client/app.ts
@@ -1,7 +1,7 @@
 $((): void =>  {
     let flamberApp: angular.IModule = angular.module('flamberApp', ['ui.router', 'ngMaterial']);
     
-    flamberApp.controller("HomeController", main.controllers.HomeController);
+    //flamberApp.controller("HomeController", main.controllers.HomeController);
     
     flamberApp.config(['$urlRouterProvider', '$stateProvider', ($urlRouterProvider: ng.ui.IUrlRouterProvider,
                                                                 $stateProvider: ng.ui.IStateProvider) => {
@@ -9,8 +9,7 @@ $((): void =>  {
         $urlRouterProvider.otherwise("/");
         $stateProvider.state('home', {
             url: "/",
-            templateUrl: "static/client/partials/home.html",
-            controller: "HomeController as HomeCtrl"
+            templateUrl: "static/client/partials/home.html"
         })
         .state('about', {
             url: "/about",
