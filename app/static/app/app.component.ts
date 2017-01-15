8611bbecc19bf5df20d759b2a73632c13a1cc087
cinkowski <cinq2ez@gmail.com>
2017-01-15 20:28:24 +0100

    Add basic toasts when logging in/error when logging in

--- app/static/app/app.component.ts
+++ app/static/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { ToastsManager } from 'ng2-toastr/ng2-toastr';
+import { Component, ViewContainerRef } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+	title = 'app works!';
+
+	constructor(public toastr: ToastsManager, vRef: ViewContainerRef) {
+		this.toastr.setRootViewContainerRef(vRef);
+	}
 }
--- app/static/app/components/login/login.component.ts
+++ app/static/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { UserService } from './../../services/user.service';
 import { Component, OnInit } from '@angular/core';
+import { ToastsManager } from 'ng2-toastr/ng2-toastr';
 
 @Component({
 	selector: 'app-login',
@@ -10,12 +11,21 @@ export class LoginComponent implements OnInit {
 	public username: string;
 	public password: string;
 
-	constructor(private userService: UserService) { }
+	constructor(private userService: UserService, private toastr: ToastsManager) { }
 
 	ngOnInit() {
 	}
 
 	public login() {
-		this.userService.login(this.username, this.password).subscribe(() => {});
+		this.userService.login(this.username, this.password).subscribe(() => {
+			this.toastr.success('Sucessfully logged in!');
+		},
+		(err) => {
+			this.toastr.error('Oops.. ' + err);
+		});
+	}
+
+	public logout() {
+		this.userService.logout();
 	}
 }
